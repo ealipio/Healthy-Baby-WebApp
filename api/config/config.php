3ef6945f15875cd91f836ee20e0236f78cb70923
Meka <mekatroniko@gmail.com>
2015-12-07 16:41:05 -0500

    subi el config y las imagenes

---
+++ api/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  Author: Eisson Alipio
+ *  Date: 30 Noviembre 2015 - 12:08 am
+ *  Last update: 20 Agosto 2015
+ *  function: Database configuration
+ */
+
+# STMP HOST
+#define('HOST_MAIL', 'ip_here');
+
+# MySQL Credentials :
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'esdeporv_vacuna');
+define('CLAVE', 'minsa');
+
+?>
