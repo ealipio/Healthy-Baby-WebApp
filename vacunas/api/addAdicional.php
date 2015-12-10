a1c4d13e1188e60d8d720666f29df8be1a0c1820
jfuertes <jfuertesl2@gmail.com>
2015-12-10 11:08:34 -0500

    info_adicional_complete

--- vacunas/api/addAdicional.php
+++ vacunas/api/addAdicional.php
@@ -3,12 +3,32 @@
 
 	$db  = new EissonConnect();
 	$dbh = $db->enchufalo();
+	session_start();
+	if(isset($_SESSION['id_usuario'])){
+		$data = json_decode(file_get_contents("php://input"));
+		$objDatos=$data->datos;
+		
 
-	$q = 'SELECT max(suma) as total from (SELECT count(id_dosis_vacunas) as suma from tb_dosis_vacunas
-			group by id_vacuna) as t';
+		$q = 'INSERT INTO tb_info_adicional (id_nino, peso, talla, fecha_medicion, observaciones, username, nutrientes, created_at) 
+				values (:id_nino, :peso, :talla, :fecha_medicion, :observaciones, :username, :nutrientes, CURRENT_TIMESTAMP)';
+		
+		$stmt = $dbh->prepare($q);
+		$stmt->bindParam(':id_nino',  $objDatos->id_nino, PDO::PARAM_STR);
+		$stmt->bindParam(':peso',  $objDatos->peso, PDO::PARAM_STR);
+		$stmt->bindParam(':talla',  $objDatos->talla, PDO::PARAM_STR);
+		$stmt->bindParam(':fecha_medicion',  $objDatos->fecha_medicion, PDO::PARAM_STR);
+		$stmt->bindParam(':nutrientes',  $objDatos->nutrientes, PDO::PARAM_STR);
+		$stmt->bindParam(':observaciones',  $objDatos->observaciones, PDO::PARAM_STR);
+		$stmt->bindParam(':username',  $_SESSION['id_usuario'], PDO::PARAM_STR);
 
-	//var_dump($r);
-	//echo json_encode($r);
+		$stmt->execute();
 
-	echo json_encode($r);
+		echo"Vacuna guardada correctamente.";
+	}
+	else{
+		echo"Error al intentar guardar vacuna";
+	}
+
+
+	
 ?>
